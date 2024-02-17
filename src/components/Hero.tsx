d4ebfd29ff4984a327024be067fb27cc915b122e
kihongchun5082 <kihongchun@gmail.com>
2024-02-17 11:29:03 +0900

    chronic disease1 postingAdd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,16 +7,12 @@ import profileImage from "../../public/전기홍_noBorder.png"
 export default function Hero() {
   return (
     // <section className="text-center mx-auto bg-slate-400">
-    <section className="text-center">
+    <section className='text-center'>
       <Image
-        // src="/전기홍1.jpg"
         src={profileImage}
-        alt="전기홍 사진"
-        // className="mx-auto p-1"
+        alt='전기홍 사진'
         className=" rounded-full mx-auto"
-        // width={80}
         width={250}
-        // height={100}
         height={250}
         priority
       />
@@ -29,11 +25,11 @@ export default function Hero() {
         {'예방의학을 전공했습니다. 보건정책과 관리가 전문분야입니다.'}
       </h3>
       {/* <p className=" text-sm">저의 바람은 더 많은 사람들이 노년을 건강하고 행복하게 지내는겁니다.</p> */}
-      <p>"저의 바람은 더 많은 사람들이 노년을 건강하고 행복하게 지내는겁니다.</p>
+      <p>저의 바람은 더 많은 사람들이 노년을 건강하고 행복하게 지내는겁니다.</p>
       {/* <p className=" text-sm">철학적이고 어려운 주제지만 영원히 추구해야 할 가치라고 생각해요.</p> */}
-      <p>철학적이고 어려운 주제지만 영원히 추구해야 할 가치라고 생각합니다."</p>
+      <p>철학적이고 어려운 주제지만 영원히 추구해야 할 가치라고 생각합니다.</p>
       {/* </div> */}
-      <Link href="/contact">
+      <Link href='/contact'>
         {/* <button className="p-1 text-sm font-bold rounded-lg bg-yellow-500 border-violet-900 border-2">이멜 주소</button> */}
         <button className="font-bold rounded-xl bg-blue-500 py-1 px-4 mt-2">
           이멜 주소
